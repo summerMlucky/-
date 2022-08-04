bb8158d553f30c72bcb263a2fd864ca66e1efd0d
summer <intoyingyingmiao@163.com>
2022-08-04 11:18:30 +0800

    公共部分完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,6 +3,25 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 const baseUrl = "http://www.liulongbin.top:3007";
 $.ajaxPrefilter((option) => {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = baseUrl + option.url;
-  });
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  if (option.url.includes("/my/")) {
+    option.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
+  option.url = baseUrl + option.url;
+
+  // 不论成功还是失败，最终都会调用 complete 回调函数
+  option.complete = (res) => {
+    console.log(res);
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      //  强制清空 token
+      localStorage.removeItem("token");
+      // 强制跳转到登录页面
+      location.href = "/login.html";
+    }
+  };
+});
---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+const getUserInfo = () => {
+  $.ajax({
+    type: "GET",
+    url: "/my/userinfo",
+    data: null,
+    success: (res) => {
+      if (res.status !== 0) return layui.layer.msg("数据请求失败！");
+      console.log(res);
+      renderAvatar(res.data);
+    },
+  });
+};
+
+// 渲染用户头像
+const renderAvatar = (data) => {
+  // 获取用户名字
+  let name = data.nickname || data.username;
+  // 设置欢迎文本
+  $("#welcome").html("欢迎" + name);
+  // 按需渲染用户头像
+  if (data.user_pic !== null) {
+    // 渲染图片头像
+    $(".layui-nav-img").attr("src", data.user_pic);
+    $(".text-avatar").hide();
+  } else {
+    // 渲染文本头像
+    $(".layui-nav-img").hide();
+    let firstName = name[0].toUpperCase();
+    $(".text-avatar").html(firstName);
+  }
+};
+getUserInfo();
+
+$('#exitBtn').click(function(){
+    // layer.confirm('is not?', {icon: 3, title:'提示'}, function(index){
+    //     //do something
+    //     layer.close(index);
+    //   });
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "" },
+        function (index) {
+            // 清空本地存储里面的 token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "/login.html";
+            console.log(index);
+        }
+    );
+})
